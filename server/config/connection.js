2f5d881bb42f2820580218b91f0689c3f4eec5f4
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-08-02 22:18:31 -0400

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// changed the database name
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Books', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
